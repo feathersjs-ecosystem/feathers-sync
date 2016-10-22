efb73cd8ff2c280a5bd08c470dacc542342d662f
Cory Smith <cory.m.smith@gmail.com>
2016-10-22 11:33:19 -0600

    jshint —> semistandard (#34)
    * jshint —> semistandard
    
    * Setting a 10 second timeout so test doesn't fail

--- src/amqp.js
+++ src/amqp.js
@@ -2,40 +2,38 @@ var amqplib = require('amqplib/callback_api');
 var debug = require('debug')('feathers-sync');
 
 var channel;
-var conf ={};
+var conf = {};
 var listSubscriber = [];
 
 var exchangeName = 'feathers-sync';
 
-var onmessage = function(ev,data){
-  if(listSubscriber[ev] && Array.isArray(listSubscriber[ev])){
-    listSubscriber[ev].forEach(function(cb){
+var onmessage = function (ev, data) {
+  if (listSubscriber[ev] && Array.isArray(listSubscriber[ev])) {
+    listSubscriber[ev].forEach(function (cb) {
       cb(data);
     });
   }
-
 };
 
-var connection = function(cb){
-
-  amqplib.connect(conf.uri, conf.amqpConnectionOptions, function (err, conn){
-    if(err){
+var connection = function (cb) {
+  amqplib.connect(conf.uri, conf.amqpConnectionOptions, function (err, conn) {
+    if (err) {
       debug('Error while connecting to AMQP: ', err.toString());
-    }else{
+    } else {
       channel = conn.createChannel();
 
       channel.assertExchange(exchangeName, 'fanout', {durable: false});
-      channel.assertQueue('', {exclusive: true}, function(err, q) {
-        if(err){
+      channel.assertQueue('', {exclusive: true}, function (err, q) {
+        if (err) {
           debug('Error while assertQueue to AMQP: ', err.toString());
-        }else{
+        } else {
           channel.bindQueue(q.queue, exchangeName, '');
-           if(typeof cb === 'function'){
-             cb(channel);
-           }
-          channel.consume(q.queue, function(msg) {
+          if (typeof cb === 'function') {
+            cb(channel);
+          }
+          channel.consume(q.queue, function (msg) {
             var temp = JSON.parse(msg.content.toString());
-            onmessage(temp.ev,temp.data);
+            onmessage(temp.ev, temp.data);
           }, {noAck: true});
         }
       });
@@ -43,74 +41,70 @@ var connection = function(cb){
   });
 };
 
-var subscribe = function(ev,cb){
-  if(listSubscriber[ev]){
+var subscribe = function (ev, cb) {
+  if (listSubscriber[ev]) {
     listSubscriber[ev].push(cb);
-  }else{
+  } else {
     listSubscriber[ev] = [cb];
   }
 };
 
-var publish = function(ev,data){
+var publish = function (ev, data) {
   var msg = {};
   msg.ev = ev;
   msg.data = data;
-  //amqp name
-  if(channel){
+  // amqp name
+  if (channel) {
     channel.publish(exchangeName, '', new Buffer(JSON.stringify(msg)));
-  }else{
-    connection(function(channel){
+  } else {
+    connection(function (channel) {
       channel.publish(exchangeName, '', new Buffer(JSON.stringify(msg)));
     });
   }
-
 };
 
-module.exports = function(config) {
-
+module.exports = function (config) {
   debug('setting up AMQP uri %s', config.uri);
   conf.uri = config.uri;
-  if(!config.prefix){
+  if (!config.prefix) {
     conf.prefix = '';
-  }else{
+  } else {
     conf.prefix = config.prefix;
-    exchangeName = conf.prefix+'-feathers-sync';
+    exchangeName = conf.prefix + '-feathers-sync';
   }
 
   conf.amqpConnectionOptions = {
-    //heartbeat: 30
+    // heartbeat: 30
   };
 
-  if(config.amqpConnectionOptions){
+  if (config.amqpConnectionOptions) {
     conf.amqpConnectionOptions = config.amqpConnectionOptions;
   }
 
-
-
   connection();
 
-  return function() {
+  return function () {
     var oldSetup = this.setup;
 
-    this.setup = function() {
+    this.setup = function () {
       var result = oldSetup.apply(this, arguments);
       var services = this.services;
-      Object.keys(services).forEach(function(path) {
+      Object.keys(services).forEach(function (path) {
         var service = services[path];
-        service._serviceEvents.forEach(function(event) {
+        service._serviceEvents.forEach(function (event) {
           var ev = path + ' ' + event;
           debug('subscribing to handler %s', ev);
-          subscribe(ev, function(data) {
+          subscribe(ev, function (data) {
             debug('got event, calling old emit %s', ev);
-            service._emit.call(service, event, data);
+            service._emit.call(service, event, data); // eslint-disable-line no-useless-call
           });
         });
       });
       return result;
     };
 
-    this.providers.push(function(path, service) {
-      if(typeof service.emit !== 'function' || typeof service.on !== 'function') {
+    this.providers.push(function (path, service) {
+      if (typeof service.emit !== 'function' || typeof service.on !== 'function') {
         return;
       }
 
@@ -119,14 +113,12 @@ module.exports = function(config) {
 
       // Override an emit that publishes to the hub
       service.mixin({
-        emit: function(ev, data) {
+        emit: function (ev, data) {
           var event = path + ' ' + ev;
           debug('emitting event to channel %s', event);
           return publish(event, data);
         }
       });
     });
-
-
   };
 };
--- src/sync.js
+++ src/sync.js
@@ -3,28 +3,27 @@ var mongo = require('./mongodb');
 var redis = require('./redis');
 var amqp = require('./amqp');
 
-module.exports = function(config) {
-
+module.exports = function (config) {
   var proto = '';
   config = config || { db: 'mongodb://localhost:27017/sync' };
 
-  if(config.db){
+  if (config.db) {
     proto = config.db.split('://')[0];
-  }else if(config.uri){
+  } else if (config.uri) {
     proto = config.uri.split('://')[0];
   }
 
-  if(['mongodb', 'redis','amqp'].indexOf(proto) === -1){
+  if (['mongodb', 'redis', 'amqp'].indexOf(proto) === -1) {
     return debug('Adapter not found %s', proto);
   }
 
   debug('will sync via adapter: %s ', proto);
 
-  if(proto === 'mongodb'){
+  if (proto === 'mongodb') {
     return mongo(config);
-  } else if(proto === 'redis') {
+  } else if (proto === 'redis') {
     return redis(config);
-  }else if(proto === 'amqp'){
+  } else if (proto === 'amqp') {
     return amqp(config);
   } else {
     return;
