fdbf45012e57f773c01bc32ab2ef875b8a990b83
David Luecke <daff@neyeon.com>
2018-03-05 10:49:25 -0800

    Add refactored implementation of AMQP

--- test/amqp.test.js
+++ test/amqp.test.js
@@ -1,17 +1,52 @@
-// const assert = require('assert');
-// const _app = require('./app');
+const assert = require('assert');
+const _app = require('./app');
+const createApp = _app('amqp', {
+  uri: 'amqp://guest:guest@localhost:5672'
+});
 
-// describe('feathers-sync RabbitMQ tests', () => {
-//   const createApp = _app('amqp', {
-//     uri: 'amqp://guest:guest@localhost:5672'
-//   });
-//   let app1, app2, app3;
+describe('feathers-sync AMQP tests', () => {
+  let app1, app2, app3;
 
-//   before(() => {
-//     app1 = createApp();
-//     app2 = createApp();
-//     app3 = createApp();
+  before(() => {
+    app1 = createApp();
 
-//     return app3.sync.ready;
-//   });
-// });
+    return app1.sync.ready.then(() => {
+      app2 = createApp();
+
+      return app2.sync.ready;
+    }).then(() => {
+      app3 = createApp();
+
+      return app3.sync.ready;
+    });
+  });
+
+  it('creating todo on app1 trigger created on all apps with hook context', done => {
+    const original = { test: 'data' };
+    let count = 0;
+    const onCreated = app => {
+      app.service('todo').once('created', (data, context) => {
+        assert.deepEqual(original, data);
+        assert.ok(context);
+        assert.deepEqual(context.result, data);
+        assert.equal(context.method, 'create');
+        assert.equal(context.type, 'after');
+        assert.equal(context.service, app.service('todo'));
+        assert.equal(context.app, app);
+
+        count++;
+        if (count === 3) {
+          done();
+        }
+      });
+    };
+
+    onCreated(app1);
+    onCreated(app2);
+    onCreated(app3);
+
+    app1.service('todo').create(original).then(data =>
+      assert.deepEqual(original, data)
+    ).catch(done);
+  });
+});
