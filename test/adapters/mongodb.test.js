3347681c2de39f8b8cd22d75cb4a4d5597dfe496
David Luecke <daff@neyeon.com>
2018-08-04 17:56:11 -0700

    Send safely stringified data to MongoDB (#85)
    * Added test to demonstrate https://github.com/feathersjs-ecosystem/feathers-sync/issues/83
    
    * Send safely stringified data to MongoDB

--- test/adapters/mongodb.test.js
+++ test/adapters/mongodb.test.js
@@ -54,4 +54,35 @@ describe('feathers-sync MongoDB tests', () => {
       assert.deepEqual(original, data)
     ).catch(done);
   });
+
+  it('updating todo on app1 with query operators trigger update on all apps with hook context', done => {
+    const original = { test: 'data' };
+    const query = { field: { $lt: 3 } };
+    let count = 0;
+    const onUpdated = app => {
+      app.service('todo').once('updated', (data, context) => {
+        assert.deepEqual(original, data);
+        assert.ok(context);
+        assert.deepEqual(context.result, data);
+        assert.equal(context.method, 'update');
+        assert.equal(context.type, 'after');
+        assert.equal(context.service, app.service('todo'));
+        assert.equal(context.app, app);
+        assert.deepEqual(context.params, { query });
+
+        count++;
+        if (count === 3) {
+          done();
+        }
+      });
+    };
+
+    onUpdated(app1);
+    onUpdated(app2);
+    onUpdated(app3);
+
+    app1.service('todo').update(null, { test: 'data' }, { query }).then(data =>
+      assert.deepEqual(original, data)
+    ).catch(done);
+  });
 });
